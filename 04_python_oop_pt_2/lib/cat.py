2b2258e977d41651ba6ed862be629efab89498d1
Matteo Piccini <gnappo.1@live.it>
2024-07-17 12:48:43 -0700

    end of lecture code

--- 04_python_oop_pt_2/lib/cat.py
+++ 04_python_oop_pt_2/lib/cat.py
@@ -1,35 +1,49 @@
 # import ipdb
 
 # 7. ✅. Create a subclass of Pet called Cat
-        # import Pet from lib.pet
+# import Pet from lib.pet
 
 from pet import Pet
 
 # 7. ✅. Create a subclass of Pet called Cat
-    
-    # import Pet from lib.pet
-class Cat(Pet):
 
-# 8. ✅. Create Cat class + __init__ that takes all the parameters from Pet and an
-# additional parameter called indoor (Boolean)
+# import Pet from lib.pet
+# "is a?"
+class A: pass
+class B: pass
+class C: pass
+
+class Cat(Pet, A, B, C):
+    all = []
+    # 8. ✅. Create Cat class + __init__ that takes all the parameters from Pet and an
+    # additional parameter called indoor (Boolean)
 
     # Use super to pass the Pet parameters to the super class (DRY)
 
     # Add an indoor attribute
 
-    def __init__(self):
-        pass
+    def __init__(self, outdoor, **kwargs):
+        super().__init__(**kwargs) #! kwargs is a dict
+        self.outdoor = outdoor
 
-# 9. ✅. Create a method unique to the Cat subclass called talk which returns the string "Meow!"
+    # 9. ✅. Create a method unique to the Cat subclass called talk which returns the string "Meow!"
 
-    def meow(self):
-        pass
-    
-# 10. ✅. Create a method called print_pet_details to match the print_pet_details in Pet    
-        
-        # Add super().print_pet_details() and print the indoor attribute
+    # def meow(self):
+    #     pass
+
+    # 10. ✅. Create a method called print_pet_details to match the print_pet_details in Pet
     def print_pet_details(self):
-        pass
-    
-# spidey = Cat(True, name="Fido", age=2, breed="pug", temperament="docile", owner="Matteo")
+        print(
+            f"""
+            name:{self.name}
+            age:{self.age}
+            breed:{self.breed}
+            outdoor:{self.outdoor}
+        """
+        )
+# Add super().print_pet_details() and print the indoor attribute
+# def print_pet_details(self):
+#     pass
 
+spidey = Cat(outdoor=True, name="Spidey", age=2, breed="persian", temperament="docile", owner="Matteo")
+print('done')
--- 04_python_oop_pt_2/lib/pet.py
+++ 04_python_oop_pt_2/lib/pet.py
@@ -4,30 +4,41 @@
 # import ipdb
 from decorator_profiler import measure_performance
 
+
 class Pet:
     # ✅ Define a class attribute (total_pets) and set it to 0
-
+    allowed_list = ["name", "age", "temperament", "breed", "owner"]
+    all = []
     # __slots__= ('_name', '_age', '_temperament', '_breed', '_owner')
 
     # What happens with our instances when we add a class attribute?
-
-    def __init__(self, name, age, breed, temperament, owner):
+    def __init__(self, *args, **kwargs):
         # In the following lines, by simply typing 'self.name = '
         # python will check if on the instance there are any setter properties
         # why setters? because there is an = after the word 'name'
         # if not, it will set the attribute manually and call it 'name'
-        self.name = name
-        self.age = age 
-        self.breed = breed
-        self.temperament = temperament
-        self.owner = owner
+        # self.name = name
+        # self.age = age
+        # self.breed = breed
+        # self.temperament = temperament
+        # self.owner = owner
+        for key, value in kwargs.items():
+            # if key in self.allowed_list: #! LEAST FAVORITE
+            # if key in Pet.allowed_list: #! SECOND LEAST FAVORITE -> ISSUE WITH INHERITANCE
+            if key in type(self).allowed_list:  # PREFERRED WAY
+                # if key in self.__class__.allowed_list: # PREFERRED WAY
+                setattr(self, key, value)
+        type(self).all.append(self)
 
     # Using Property to control the behavior of attributes
-    def get_name(self):
+
+    @property
+    def name(self):
         print("Inside the name property getter")
         return self._name
 
-    def set_name(self, new_name):
+    @name.setter
+    def name(self, new_name):
         print("Inside the name property setter")
         if not isinstance(new_name, str):
             raise TypeError("Value must be a string")
@@ -36,28 +47,30 @@ class Pet:
         else:
             self._name = new_name
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    # name = property(get_name, set_name)
+    @property
+    def breed(self):
         print("Inside the breed property getter")
         return self._breed
 
-    def set_breed(self, new_breed):
+    @breed.setter
+    def breed(self, new_breed):
         print("Inside the breed property setter")
         if not isinstance(new_breed, str):
             raise TypeError("Value must be a string")
-        elif len(new_breed) <=2:
+        elif len(new_breed) <= 2:
             raise ValueError("Value must be an int greater than 0")
         else:
             self._breed = new_breed
 
-    breed = property(get_breed, set_breed)
-
-    def get_age(self):
+    # breed = property(get_breed, set_breed)
+    @property
+    def age(self):
         print("Inside the age property getter")
         return self._age
 
-    def set_age(self, new_age):
+    @age.setter
+    def age(self, new_age):
         print("Inside the age property setter")
         if not isinstance(new_age, int):
             raise TypeError("Value must be an int")
@@ -66,13 +79,14 @@ class Pet:
         else:
             self._age = new_age
 
-    age = property(get_age, set_age)
-
-    def get_owner(self):
+    # age = property(get_age, set_age)
+    @property
+    def owner(self):
         print("Inside the owner property getter")
-        raise AttributeError('Privacy concern, you cannot see me!')
+        raise AttributeError("Privacy concern, you cannot see me!")
 
-    def set_owner(self, new_owner):
+    @owner.setter
+    def owner(self, new_owner):
         print("Inside the owner property setter")
         if not isinstance(new_owner, str):
             raise TypeError("Value must be an string")
@@ -81,13 +95,14 @@ class Pet:
         else:
             self._owner = new_owner
 
-    owner = property(get_owner, set_owner)
-
-    def get_temperament(self):
+    # owner = property(get_owner, set_owner)
+    @property
+    def temperament(self):
         print("Inside the temperament property getter")
-        raise AttributeError('Privacy concern, you cannot see me!')
+        raise AttributeError("Privacy concern, you cannot see me!")
 
-    def set_temperament(self, new_temperament):
+    @temperament.setter
+    def temperament(self, new_temperament):
         print("Inside the temperament property setter")
         if not isinstance(new_temperament, str):
             raise TypeError("Value must be a string")
@@ -96,17 +111,46 @@ class Pet:
         else:
             self._temperament = new_temperament
 
-    temperament = property(get_temperament, set_temperament)
+    @temperament.deleter
+    def temperament(self):
+        del self._temperament
 
-    def __repr__(self):
-        print(f'''
+    # temperament = property(get_temperament, set_temperament)
+
+    def print_pet_details(self):
+        print(
+            f"""
             name:{self.name}
             age:{self.age}
             breed:{self.breed}
-            temperament:{self.temperament}
-            owner:{self.owner}
-        ''')
+        """
+        )
 
-fido = Pet(name="Fido", age=2, breed="pug", temperament="docile", owner="Matteo")
+    @classmethod
+    def find_by_name(cls, name_to_find):
+        #! WARNING DO NOT USE
+        # matches = [
+        #     pet for pet in cls.all if pet.name.lower() == name_to_find.lower()
+        # ]
+        # return matches[0] if matches else None
+        #! PLEASE USE BELOW
+        # for pet in cls.all:
+        #     if pet.name.lower() == name_to_find.lower():
+        #         return pet
+
+        # return False
+        #! PERFECTION TO ASPIRE TO
+        return next(
+            (pet for pet in cls.all if pet.name.lower() == name_to_find.lower()),
+            None,
+        )
+
+    @staticmethod
+    def sum(x, y):
+        return x + y
+
+fido = Pet(
+    name="Fido", age=2, breed="pug", temperament="docile", owner="Matteo", test="test"
+)
 milo = Pet(name="Milo", age=2, breed="pug", temperament="docile", owner="Matteo")
-print('done')
+print("done")
