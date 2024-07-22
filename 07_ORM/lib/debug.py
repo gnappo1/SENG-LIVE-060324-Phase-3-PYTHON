78da2ef9b1606904b4a63fb7599e83d5eae631b7
Matteo Piccini <gnappo.1@live.it>
2024-07-22 13:00:11 -0700

    add solution code for part 1

--- 07_ORM/lib/debug.py
+++ 07_ORM/lib/debug.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-from owner import Owner, CONN as owner_conn, CURSOR as owner_cursor
+# from owner import Owner, CONN as owner_conn, CURSOR as owner_cursor
 from pet import Pet, CONN as pet_conn, CURSOR as pet_cursor
 
 # Owner.create_table()
 # frank = Owner("frank", "555-555-5555", "frank@gmail.com", "555 Somewhere St.")
 # frank.save()
 
-# Pet.create_table()
-# spot = Pet("spot", "dog", "chihuahua", "feisty")
+Pet.drop_table()
+Pet.create_table()
+spot = Pet.create("fido", "dog", "chihuahua", "feisty")
 # spot.save()
 
 import ipdb; ipdb.set_trace()
--- 07_ORM/lib/pet.py
+++ 07_ORM/lib/pet.py
@@ -6,26 +6,86 @@
 
 # https://docs.python.org/3/library/sqlite3.html#tutorial
 import sqlite3
-
+from helper import Helper
 # https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection
 CONN = sqlite3.connect('lib/resources.db')
 
 # https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor
 CURSOR = CONN.cursor()
 
-class Pet:
-
-    pass
+class Pet(Helper): #mixin
+    all_ = {}
 
     # ✅ 1. Add "__init__" with "name", "species", "breed", "temperament", and "id" (Default: None) Attributes
+    def __init__(self, name, species, breed, temperament, id=None):
+        self.name = name
+        self.species = species
+        self.breed = breed
+        self.temperament = temperament
+        self.id = id
 
     # ✅ 2. Add "create_table" Class Method to Create "pets" Table If Doesn't Already Exist
+    @classmethod
+    def create_table(cls):
+        try:
+            with CONN:
+                CURSOR.execute(f"""
+                    CREATE TABLE IF NOT EXISTS {cls.pascal_to_camel_plural()} (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        species TEXT,
+                        breed TEXT,
+                        temperament TEXT
+                    );
+                """)
+        # except sqlite3.OperationalError as e:
+        #     print(e)
+        except sqlite3.IntegrityError as e:
+            return e
 
     # ✅ 3. Add "drop_table" Class Method to Drop "pets" Table If Exists
+    @classmethod
+    def drop_table(cls):
+        try:
+            with CONN:
+                CURSOR.execute(
+                    f"""
+                        DROP TABLE IF EXISTS {cls.pascal_to_camel_plural()}
+                    """
+                )
+        # except sqlite3.OperationalError as e:
+        #     print(e)
+        except sqlite3.IntegrityError as e:
+            return e
 
     # ✅ 4. Add "save" Instance Method to Persist New "pet" Instances to DB
+    def save(self):
+        try:
+            with CONN:
+                CURSOR.execute(
+                    f"""
+                        INSERT INTO {type(self).pascal_to_camel_plural()}
+                        (name, species, breed, temperament)
+                        VALUES
+                        (?, ?, ?, ?)
+                """, (self.name, self.species, self.breed, self.temperament)
+                )
+                self.id = CURSOR.lastrowid
+                type(self).all_[self.id] = self
+
+        # except sqlite3.OperationalError as e:
+        #     print(e)
+        except sqlite3.IntegrityError as e:
+            return e
 
     # ✅ 5. Add "create" Class Method to Initialize and Save New "pet" Instances to DB
+    @classmethod
+    def create(cls, name, species, breed, temperament):
+        #! first instantiate object
+        pet = cls(name, species, breed, temperament)
+        #! now persist to the db using save()
+        pet.save()
+        # return pet
 
     # ✅ 6. Add "update" Instance Method to Update the db based on its Attributes
 
@@ -46,3 +106,9 @@ class Pet:
     #  Find and Retrieve "pet" Instance w/ All Attributes
 
     # If No "pet" Found, Create New "pet" Instance w/ All Attributes
+
+
+fido = Pet("fido", "dog", "dalmatian", "docile")
+
+# instantiation means initializing a new object out of the class
+# creation implies PERSISTENCE -> something is inserted into the db
